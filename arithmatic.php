05d8a4f7decb2591cbfface7ead619962a67e7f5
Shivakant Vishwakarma <30767792+shivakant1999@users.noreply.github.com>
2019-06-07 18:07:23 +0530

    updated file

--- arithmatic.php
+++ arithmatic.php
@@ -13,6 +13,16 @@ function sub($a,$b)
     $sub=$a-$b;
     echo "<h3>substraction is: $sub</h3>";
 }
+function mul($a,$b)
+{
+    $multiply=$a*$b;
+    echo "<h3>Multiplication is: $multiply</h3>";
+}
+function div($a,$b)
+{
+    $division=$a/$b;
+    echo "<h3>Division is: $division</h3>";
+}
 if(isset($_POST['add']))
 {
     add($_POST['first'],$_POST['second']);
@@ -20,6 +30,14 @@ if(isset($_POST['add']))
 if(isset($_POST['sub']))
 {
     sub($_POST['first'],$_POST['second']);
+}
+if(isset($_POST['mul']))
+{
+    mul($_POST['first'],$_POST['second']);
+}
+if(isset($_POST['div']))
+{
+    div($_POST['first'],$_POST['second']);
 }
 ?>
 </html>
