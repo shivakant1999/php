011dc86efdef7859b3a9d0dbe97eff3321f8e940
Shivakant Vishwakarma <30767792+shivakant1999@users.noreply.github.com>
2019-06-08 18:29:07 +0530

    Form using php

---
+++ form/welcome.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<?php
+$name=$_POST["exampleInputEmail1"];
+$password=$_POST["exampleInputPassword1"];
+
+echo "<h3>Welcome $name</h3>";
+?>
+</html>
